3d235da5a5599db51f54fc445bc1e746466b437e
Joel Simo <joelsimooverride@gmail.com>
2025-04-01 11:18:03 +0100

    adding reason code

---
+++ src/Support/Constants/BcReasonCode.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Kirago\BusinessCore\Support\Constants;
+
+enum BcReasonCode : string
+{
+
+    case ORGANIZATION_NOT_FOUND            = "ORGANIZATION_NOT_FOUND";
+    case ORGANIZATION_INACTIVE             = "ORGANIZATION_INACTIVE";
+    case REQUIRED_X_ORGANIZATION_ID_HEADER = "REQUIRED_X_ORGANIZATION_ID_HEADER";
+    case UNABLE_TO_GET_DRIVER              = "UNABLE_TO_GET_DRIVER";
+    case UAPPLICATION_ERROR                = "APPLICATION_ERROR";
+
+
+}
